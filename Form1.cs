3c643b186e2d6b3f9f8abf4a777baa8fba069ca5
antoine baronnet <antoine.baronnet@outlook.com>
2023-11-12 13:04:06 +0100

    commit#3 Utilisation try catch / exception

--- Form1.cs
+++ Form1.cs
@@ -28,78 +28,98 @@ namespace TP_Socket
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            if (socket != null)
+            try
             {
-                MessageBox.Show("Le socket est déjà initialisé.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
+                if (socket != null)
+                {
+                    MessageBox.Show("Le socket est déjà initialisé.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                IPEndPoint iped = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+
+                if (!socket.ConnectAsync(new SocketAsyncEventArgs { RemoteEndPoint = iped }))
+                {
+                    MessageBox.Show($"Erreur lors de la connexion au serveur : {socket.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Error)}", "Erreur de connexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    socket.Close();
+                    socket = null;
+                }
             }
-
-            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            IPEndPoint iped = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
-
-            if (!socket.ConnectAsync(new SocketAsyncEventArgs { RemoteEndPoint = iped }))
+            catch (SocketException se)
             {
-                MessageBox.Show($"Erreur lors de la connexion au serveur : {socket.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Error)}", "Erreur de connexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-                socket.Close();
-                socket = null; 
+                MessageBox.Show($"Erreur lors de la connexion au serveur : {se.Message}", "Erreur de connexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
         }
-
-
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (socket != null && socket.Connected)
+            try
             {
-                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 5000);
+                if (socket != null && socket.Connected)
+                {
+                    socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 5000);
 
-                var messageEnvoi = Encoding.ASCII.GetBytes("GET /\r\n\r\n");
-                socket.Send(messageEnvoi);
+                    var messageEnvoi = Encoding.ASCII.GetBytes("GET /\r\n\r\n");
+                    socket.Send(messageEnvoi);
 
- 
-                textBox3.Text = Encoding.ASCII.GetString(messageEnvoi);
+                    textBox3.Text = Encoding.ASCII.GetString(messageEnvoi);
+                }
+                else
+                {
+                    MessageBox.Show("Le socket n'est pas initialisé ou n'est pas connecté.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
-            else
+            catch (SocketException se)
             {
-                MessageBox.Show("Le socket n'est pas initialisé ou n'est pas connecté.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Erreur lors de l'envoi de données : {se.Message}", "Erreur d'envoi", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void buttonRecevoir_Click(object sender, EventArgs e)
         {
-            
-            if (socket != null && socket.Connected)
+            try
             {
-                var messageRecu = new byte[1024];
-                int nbcarrecu = socket.Receive(messageRecu);
-                string texteRecu = Encoding.ASCII.GetString(messageRecu, 0, nbcarrecu);
+                if (socket != null && socket.Connected)
+                {
+                    var messageRecu = new byte[1024];
+                    int nbcarrecu = socket.Receive(messageRecu);
+                    string texteRecu = Encoding.ASCII.GetString(messageRecu, 0, nbcarrecu);
 
-                this.textBoxReception.Text = $"Nombre de caractères reçus : {nbcarrecu}\n{texteRecu}";
+                    this.textBoxReception.Text = $"Nombre de caractères reçus : {nbcarrecu}\n{texteRecu}";
+                }
+                else
+                {
+                    MessageBox.Show("Le socket n'est pas initialisé ou n'est pas connecté.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
-            else
+            catch (SocketException se)
             {
-                MessageBox.Show("Le socket n'est pas initialisé ou n'est pas connecté.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Erreur lors de la réception de données : {se.Message}", "Erreur de réception", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
 
         private void button4_Click(object sender, EventArgs e)
         {
-
-            if (socket != null)
+            try
             {
-
-                socket.Close();
+                if (socket != null)
+                {
+                    socket.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Le socket n'est pas initialisé.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
-            else
+            catch (SocketException se)
             {
-                MessageBox.Show("Le socket n'est pas initialisé.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Erreur lors de la fermeture du socket : {se.Message}", "Erreur de fermeture", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
 
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -117,11 +137,20 @@ namespace TP_Socket
         {
 
         }
+        private void label2_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+         {
+
+         }
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
             texteMessage = textBox3.Text;
         }
 
+        
     }
 }
